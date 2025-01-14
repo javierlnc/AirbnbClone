713543e189c71e5b539ac2d7c7a5d20e57d2b0fc
javierlnc <javierlnc@icloud.com>
2025-01-13 21:40:38 -0500

    Add Navbar component with logo and update SVG asset

--- airbnb/components/Navbar.tsx
+++ airbnb/components/Navbar.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link"
+import Image from "next/image"
+
 const Navbar = () => {
   return <header className="w-full fixed top-0 left-0 border-b py-6 bg-white z-10">
-    <nav>
-      AirBnb
+    <nav className="max-w-[1500px] mx-auto px-6">
+      <div className="flex justify-between items-center">
+        <Link href={'/'}>
+        <Image src={'/airbnb-1.svg'}
+        width={36}
+        height={54}
+        alt="Home Page"/>
+        </Link>
+        
+      </div>
+
     </nav>
     <div>
 
