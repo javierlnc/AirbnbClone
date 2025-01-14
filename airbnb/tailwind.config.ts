6ee86515d3e2e93eda708f0e0521748428db8a01
javierlnc <javierlnc@icloud.com>
2025-01-13 19:50:11 -0500

    Initialize Next.js project with Tailwind CSS and add essential configuration files

---
+++ airbnb/tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        airbnb:{
+          'neutral': '#FF5A5F',
+          'dark': '#C7494E'
+        }
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
